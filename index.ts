f9648b2c2c02b7a9fa1f6040585eb5748d938436
Virajj <viraj28j@gmail.com>
2022-08-01 22:55:04 +0530

    datasource creds used in server file

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
+// import { AppDataSource } from "./src/db/data-source";
 import startServer from "./src/server";
 
 startServer();
+
+// AppDataSource.initialize()
+//  .then((dataSource) => {
+//     console.log("Data source initialized");
+//     const service = new RegisterServices(dataSource);
+//     startServer(service)
+//  })
+//  .catch((err) => {
+//     console.log("Error initializing data source");
+//  })
